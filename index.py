9602e4dba352f31c6c23e010a4e5ebd329563b24
Bobek, Richard <richard.bobek@merck.com>
2022-03-14 22:59:50 +0100

    fun

--- index.py
+++ index.py
@@ -8,23 +8,14 @@ from tabs import intro, predict, explain, evaluate
 style = {'maxWidth': '960px', 'margin': 'auto'}
 
 app.layout = html.Div([
-    dcc.Markdown('# Multiple Offer Calculator'),
-    dcc.Tabs(id='tabs', value='tab-intro', children=[
-        dcc.Tab(label='Intro', value='tab-intro'),
-        dcc.Tab(label='Predict', value='tab-predict'),
-        dcc.Tab(label='Explain', value='tab-explain'),
-        dcc.Tab(label='Evaluate', value='tab-evaluate'),
-    ]),
+    dcc.Markdown('Do you mind Vladimir Putin to become Russian president again?'),
     html.Div(id='tabs-content'),
 ], style=style)
 
-@app.callback(Output('tabs-content', 'children'),
-              [Input('tabs', 'value')])
-def render_content(tab):
-    if tab == 'tab-intro': return intro.layout
-    elif tab == 'tab-predict': return predict.layout
-    elif tab == 'tab-explain': return explain.layout
-    elif tab == 'tab-evaluate': return evaluate.layout
+dcc.RadioItems(
+   options=['YES. I don not mind', 'NO. I don not mind.'],
+   value='YES. I don not mind'
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
